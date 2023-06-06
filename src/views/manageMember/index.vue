39ef846a81a8273c82405ad18bf1feec2d5042f1
Mars Y Huang <mars_y_huang@trendmicro.com>
2023-06-06 21:30:27 +0800

    Finish main layout and basic data binding, table not yet.

--- src/views/manageMember/index.vue
+++ src/views/manageMember/index.vue
@@ -10,6 +10,66 @@
           <SubTitle title="會員管理" class="min-w-fit" />
         </div>
       </header>
+      <div class="grid gap-1 md:gap-2">
+        <div class="flex gap-1 md:gap-2 items-center">
+          <small class="text-gray-500 w-[100px] text-[13px]">查找會員</small>
+          <el-input
+            v-model="state.search.keyword"
+            placeholder="Search Text"
+            class="input-with-select">
+            <template #prepend>
+              <el-select
+                v-model="state.search.column"
+                placeholder="Select"
+                style="width: 115px">
+                <el-option
+                  v-for="column in state.search.columns"
+                  :key="column.value"
+                  :label="column.label"
+                  :value="column.value" />
+              </el-select>
+            </template>
+            <template #append>
+              <el-button icon="Search" />
+            </template>
+          </el-input>
+        </div>
+        <div class="border">
+          keyword: {{ state.search.keyword }}, column: {{ state.search.column }}
+        </div>
+      </div>
+      <div class="grid gap-1 md:gap-2">
+        <div class="flex gap-1 md:gap-2 items-center">
+          <small class="text-gray-500 w-[100px] text-[13px]"
+            >會員基本資料</small
+          >
+          <el-input
+            v-model="state.memberInfo.id"
+            placeholder="Member ID"
+            class="input-with-select">
+            <template #append>
+              <el-button icon="Search" />
+            </template>
+          </el-input>
+        </div>
+        <div class="border">{{ state.memberInfo.id }}</div>
+      </div>
+      <div class="grid gap-1 md:gap-2">
+        <div class="flex gap-1 md:gap-2 items-center">
+          <small class="text-gray-500 w-[100px] text-[13px]"
+            >會員營運資料</small
+          >
+          <el-input
+            v-model="state.memberData.id"
+            placeholder="Member ID"
+            class="input-with-select">
+            <template #append>
+              <el-button icon="Search" />
+            </template>
+          </el-input>
+        </div>
+        <div class="border">{{ state.memberData.id }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,8 +77,26 @@
 import { reactive, onMounted, getCurrentInstance } from 'vue'
 import { ManageMemberApiHandler } from '@/api/manage_member'
 import SubTitle from '@/components/Text/SubTitle.vue'
+import BasicTable from '@/components/Table/index.vue'
 const { proxy } = getCurrentInstance()
-const state = reactive({})
+const state = reactive({
+  search: {
+    keyword: '',
+    column: '',
+    columns: [
+      { value: '', label: '全部' },
+      { value: 'birth', label: '生日' },
+      { value: 'idnumber', label: '會員編號' },
+      { value: 'email', label: '電子信箱' },
+    ],
+  },
+  memberInfo: {
+    id: '',
+  },
+  memberData: {
+    id: '',
+  },
+})
 onMounted(() => {
   actions.handleSearchMember()
 })
